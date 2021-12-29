3c925b66789ae8df3055a8ee10b818ac445c6e1e
mateus trentz <mateus.trentz@gmail.com>
2021-12-29 14:51:09 -0300

    simple menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GameContext from "./contexts/GameContext";
 import Board from "./components/Board";
 import NextPieceMenu from "./components/NextPieceMenu";
 import ScoreMenu from "./components/ScoreMenu";
+import OptionsMenu from "./components/OptionsMenu";
 
 function App() {
   const { handleKeyPress, handleKeyRelease, handleKeyDown } = useContext(GameContext);
@@ -11,6 +12,7 @@ function App() {
     <button className="bg-gray-600 w-full h-full cursor-auto" onKeyDown={handleKeyPress} onKeyUp={handleKeyRelease}>
       <NextPieceMenu />
       <ScoreMenu />
+      <OptionsMenu />
       <Board />
     </button>
   );
---
+++ src/components/OptionsMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OptionsMenu = () => {
+  return (
+    <pre className="flex flex-row justify-center gap-5">
+      <button>Pause Game</button>
+      <button>Restart</button>
+      <button>Toggle Audio</button>
+    </pre>
+  );
+};
+
+export default OptionsMenu;

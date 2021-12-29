20c37af315da67ce5d71f0f2c515b6630aaab3d3
mateus trentz <mateus.trentz@gmail.com>
2021-12-29 20:43:08 -0300

    removed frame count

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,20 +1,16 @@
 import React, { useContext } from "react";
 import GameContext from "../contexts/GameContext";
-import TimeContext from "../contexts/TimeContext";
 import BoardTable from "./BoardTable";
 import GameOverTable from "./GameOverTable";
 
 const Board = () => {
   const { mergedBoard, gameOver } = useContext(GameContext);
-  const { counter } = useContext(TimeContext);
 
-  // TODO: Preciso achar um jeito de colocar a cor depois... Acho que vai ser hard
   return (
     <div>
       <pre className="whitespace-pre-wrap leading-4 tracking-normal md:tracking-wider">
         {gameOver ? <GameOverTable mergedBoard={mergedBoard} /> : <BoardTable mergedBoard={mergedBoard} />}
       </pre>
-      <p> Frame count - {counter}</p>
     </div>
   );
 };

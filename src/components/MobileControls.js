b95c5a50a46746de4b0c905fa2b358fd16a476c9
mateus trentz <mateus.trentz@gmail.com>
2021-12-29 23:13:45 -0300

    toggle speed up for mobile

--- src/components/MobileControls.js
+++ src/components/MobileControls.js
@@ -1,15 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import GameContext from "../contexts/GameContext";
 import TimeContext from "../contexts/TimeContext";
 
 const MobileControls = () => {
   const { movePieceRight, movePieceLeft, rotatePiece } = useContext(GameContext);
+  const { setFast, setNormal } = useContext(TimeContext);
+
+  const [isNormal, setIsNormal] = useState(true);
+
+  const toggleSpeed = () => {
+    if (isNormal) {
+      setFast();
+      setIsNormal(false);
+    } else {
+      setNormal();
+      setIsNormal(true);
+    }
+  };
+
   return (
-    <pre className="flex flex-row justify-center gap-5 lg:hidden">
+    <pre className="flex flex-row justify-center gap-4 lg:hidden">
       <div className="mt-1">Mobile Controls:</div>
-      <div className="flex flex-row justify-center gap-5">
+      <div className="flex flex-row justify-center gap-8">
         <button onClick={movePieceLeft} className="text-xl">
           {"<"}
+        </button>
+        <button onClick={toggleSpeed} className="text-xl">
+          {"v"}
         </button>
         <button onClick={rotatePiece} className="text-xl rotate-180 mt-2">
           {"v"}
